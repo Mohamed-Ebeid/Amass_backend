fd908af8caa526797feadbd50ee4181229d382f4
Mohamed Ebeid <mohmedalhkim@gmail.com>
2023-05-19 14:31:34 +0300

    Done with leave with the frontend

--- Controllers/leaveController.js
+++ Controllers/leaveController.js
@@ -1,4 +1,5 @@
 import Leave from "../Models/leave.js";
+import Employee from "../Models/employee.js";
 //Getting leave for employee
 export const getLeave = async (req, res) => {
 	try {
@@ -14,6 +15,29 @@ export const getLeave = async (req, res) => {
 		res.status(400).send("Error => " + e.message);
 	}
 };
+
+//Get leave report for admin
+export const getLeaveAdmin = async (req, res) => {
+	try {
+		const leave = await Leave.find();
+		res.send(
+			await Leave.aggregate([
+				{
+					$lookup: {
+						from: "employees",
+						localField: "emp_id",
+						foreignField: "_id",
+						as: "emp",
+					},
+				},
+				{ $project: { emp_id: 0, "emp.emp_pass": 0 } },
+			])
+		);
+	} catch (e) {
+		res.status(400).send("Error => " + e.message);
+	}
+};
+
 //Apply for a leave
 export const apply = async (req, res) => {
 	try {
@@ -41,8 +65,8 @@ export const accept = async (req, res) => {
 	try {
 		const leave = await Leave.findById(req.body.leaveId);
 		if (leave) {
-			(leave.status = "accepted"), await leave.save();
-			res.json(leave);
+			(leave.status = "Accepted"), await leave.save();
+			res.json("Accepted");
 		} else {
 			res.json("This leave does not exsits!");
 		}
@@ -57,7 +81,7 @@ export const deny = async (req, res) => {
 		const leave = await Leave.findById(req.body.leaveId);
 		if (leave) {
 			(leave.status = "Denied"), await leave.save();
-			res.json(leave);
+			res.json("Denied");
 		} else {
 			res.json("This leave does not exsits!");
 		}
--- Routes/leaveRoutes.js
+++ Routes/leaveRoutes.js
@@ -4,13 +4,15 @@ import {
 	accept,
 	deny,
 	getLeave,
+	getLeaveAdmin,
 } from "../Controllers/leaveController.js";
 import { isAuth, isAdmin } from "../utils.js";
 
 const jobRouter = express.Router();
 
 //api courses
-jobRouter.get("/", isAuth, getLeave);
+jobRouter.get("/", isAuth, getLeave); //single leave
+jobRouter.get("/all", isAuth, isAdmin, getLeaveAdmin); // all leaves for admin
 jobRouter.post("/apply", isAuth, apply); //Apply for a leave
 jobRouter.put("/accept", isAuth, isAdmin, accept); //Accept a leave
 jobRouter.put("/deny", isAuth, isAdmin, deny); //Accept a leave
