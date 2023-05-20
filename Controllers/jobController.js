a83d583e97e6d6a13208b3ceb2df4ea88bef1974
Mohamed Ebeid <mohmedalhkim@gmail.com>
2023-05-20 19:30:37 +0300

    Done with job titles with the frontend

--- Controllers/jobController.js
+++ Controllers/jobController.js
@@ -1,4 +1,32 @@
 import Job_Title from "../Models/job.js";
+import Employee from "../Models/employee.js";
+import Department from "../Models/department.js";
+
+//Get all job title
+export const getJobReport = async (req, res) => {
+	res.send(
+		await Job_Title.aggregate([
+			{
+				$lookup: {
+					from: "employees",
+					localField: "emp_id",
+					foreignField: "_id",
+					as: "emp",
+				},
+			},
+
+			{ $project: { "emp.emp_pass": 0 } },
+			{
+				$lookup: {
+					from: "departments",
+					localField: "department",
+					foreignField: "_id",
+					as: "dep",
+				},
+			},
+		])
+	);
+};
 
 //Add a job title
 export const addJob = async (req, res) => {
@@ -24,6 +52,19 @@ export const updateJob = async (req, res) => {
 			job.emp_id = req.body.emp_id;
 
 			await job.save();
+			res.json("Done");
+		} else {
+			res.json("This job title does not exsits!");
+		}
+	} catch (e) {
+		return res.status(400).send(e.message);
+	}
+};
+
+export const deleteJob = async (req, res) => {
+	try {
+		const job = await Job_Title.findByIdAndRemove(req.params.id);
+		if (job) {
 			res.json("Done");
 		} else {
 			res.json("This job title does not exsits!");
--- Routes/jobRoutes.js
+++ Routes/jobRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { addJob, updateJob } from "../Controllers/jobController.js";
+import {
+	addJob,
+	updateJob,
+	getJobReport,
+	deleteJob,
+} from "../Controllers/jobController.js";
 import { isAuth, isAdmin } from "../utils.js";
 
 const jobRouter = express.Router();
@@ -7,5 +12,7 @@ const jobRouter = express.Router();
 //api courses
 jobRouter.post("/", isAuth, isAdmin, addJob); //Add a job title
 jobRouter.put("/edit/:jobId", isAuth, isAdmin, updateJob); //update a job title
+jobRouter.get("/all", isAuth, isAdmin, getJobReport); //
+jobRouter.delete("/delete/:id", isAuth, isAdmin, deleteJob);
 
 export default jobRouter;
